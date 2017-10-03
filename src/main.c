4b23e95eb6ad72191b15649e303cd151572880b6
lex <lex@users.noreply.github.com>
2017-10-03 13:06:45 +0300

    add mandelbrot drawing

--- src/main.c
+++ src/main.c
@@ -10,21 +10,27 @@
 #include "draw.h"
 #include "memory.h"
 
-uint8_t logBuffer[0x400];
+#define SCREEN_WIDTH 480
+#define SCREEN_HEIGHT 272
+#define LOG_BUFFER_SIZE 0x400
+#define MEM1_ALIGNMENT 0x40
+
+uint8_t logBuffer[LOG_BUFFER_SIZE] = {0};
 bool initialized = false;
 bool isAppRunning = true;
+bool needToDraw = true;
 
 void initializeScreen() {
     // Grab the buffer size for each screen (TV and gamepad)
     const int sizeOfBuffer0 = OSScreenGetBufferSizeEx(0);
     const int sizeOfBuffer1 = OSScreenGetBufferSizeEx(1);
-    __os_snprintf(logBuffer, 0x400, "Screen sizes %x, %x\n", sizeOfBuffer0,
+    __os_snprintf(logBuffer, LOG_BUFFER_SIZE, "Screen sizes %x, %x\n", sizeOfBuffer0,
                   sizeOfBuffer1);
     OSReport(logBuffer);
 
     // Set the buffer area.
-    screenBuffer = MEM1_alloc(sizeOfBuffer0 + sizeOfBuffer1, 0x40);
-    __os_snprintf(logBuffer, 0x400, "Allocated screen buffers at %x\n",
+    screenBuffer = MEM1_alloc(sizeOfBuffer0 + sizeOfBuffer1, MEM1_ALIGNMENT);
+    __os_snprintf(logBuffer, LOG_BUFFER_SIZE, "Allocated screen buffers at %x\n",
                   screenBuffer);
     OSReport(logBuffer);
 
@@ -95,6 +101,39 @@ static bool AppRunning() {
     return isAppRunning;
 }
 
+void mandelbrot() {
+    const int maximumIterations = 200;
+    const uint8_t alpha = 255;
+    uint8_t color[3] = {0};
+
+    for (int row = 0; row < SCREEN_HEIGHT; ++row) {
+        for (int column = 0; column < SCREEN_WIDTH; ++column) {
+            const double cRe = ((column - SCREEN_WIDTH / 2.0f) * 4.0f / SCREEN_WIDTH);
+            const double cIm = ((row - SCREEN_HEIGHT / 2.0f) * 4.0f / SCREEN_WIDTH);
+
+            double x = 0.0f;
+            double y = 0.0f;
+
+            int iteration = 0;
+
+            while ((x * x + y * y) <= 4.0f && iteration < maximumIterations) {
+                double newX = (x * x - y * y + cRe);
+                y = (2.0f * x * y + cIm);
+                x = newX;
+                ++iteration;
+            }
+
+            const uint8_t colorValue = (iteration >= maximumIterations) ? 0 : 255;
+
+            for (int i = 0; i < 3; ++i) {
+                color[i] = colorValue;
+            }
+
+            drawPixel(column, row, color[0], color[1], color[2], alpha);
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     OSEnableHomeButtonMenu(false);
     OSScreenInit();
@@ -105,7 +144,11 @@ int main(int argc, char** argv) {
             continue;
         }
 
-        drawString(0, 0, "hello world");
+        if (needToDraw) {
+            mandelbrot();
+            flipBuffers();
+            needToDraw = false;
+        }
     }
 
     SYSRelaunchTitle(0, NULL);
